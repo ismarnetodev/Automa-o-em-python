4f5e21e0e4f60b460e03d2475840a292a744ef79
ismarnetodev <dsxzz41@gmail.com>
2025-06-11 15:51:17 -0300

    Mensagem enviada automaticamente

--- principal.py
+++ principal.py
@@ -2,6 +2,7 @@ import openpyxl
 from urllib.parse import quote
 import webbrowser
 from time import sleep
+import pyautogui
 
 webbrowser.open('https://web.whatsapp.com/')
 sleep(15)  
@@ -15,16 +16,19 @@ for linha in paginas_clientes.iter_rows(min_row=2):
     vencimento = linha[2].value
 
     if not nome or not telefone or not vencimento:
-        continue  # pula se faltar algum dado
+        continue 
 
-    # Formatação
     telefone = str(telefone).strip()
     if not telefone.startswith('+'):
-        telefone = f'+55{telefone}'  # Adiciona código do país
+        telefone = f'+55{telefone}' 
 
     mensagem = f'Olá {nome}, o seu boleto vence na data {vencimento.strftime("%d/%m/%Y")}!'
     print(f'Enviando para {nome} ({telefone}): {mensagem}')
 
     link_mensagem_wpp = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
     webbrowser.open(link_mensagem_wpp)
-    sleep(10)  # Tempo para envio manual ou leitura
+    sleep(10)
+    seta = pyautogui.press('enter')
+    sleep(10)
+    pyautogui.hotkey('ctrl','w')
+    sleep(5)
